d3e7420f8b3b3a4afa92fad18e62ce6f041aa5f9
glimmer-crypto <glimmercrypto@gmail.com>
2021-04-13 08:10:20 -0400

    Improved transaction form validation

--- web/vue-components/transaction-form.js
+++ web/vue-components/transaction-form.js
@@ -26,21 +26,27 @@ app.component("transaction-form", {
   template: document.getElementById("transaction-form-template").innerHTML,
   methods: {
     sendTransaction() {
+      if (this.invalidAddress) { return }
+
       const amount = amountFromString(this.amount)
       if (amount > app.balance.amount || isNaN(amount)) {
         this.error = "amount"
         return
       }
 
+      node.sendTransaction(amount, this.address)
+    }
+  },
+  computed: {
+    invalidAddress() {
+      if (this.address.length < 30) { return null }
+
       try {
         new glimmer.Key.Public(this.address)
+        return false
       } catch (err) {
-        console.error(err)
-        this.error = "address"
-        return
+        return true
       }
-
-      node.sendTransaction(amount, this.address)
     }
   },
   watch: {
